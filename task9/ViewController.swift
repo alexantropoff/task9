20170e81d49aeeb1f0dee4ffbb5dd2431332253e
Alex Antropoff <alantr@gmail.com>
2023-03-25 23:15:22 +0200

    Initial Commit

---
+++ task9/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  task9
+//
+//  Created by Alex Antropoff on 25.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
