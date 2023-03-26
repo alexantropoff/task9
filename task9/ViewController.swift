f99d37e5ccd4afadd87b492d25b631c5a9c99f35
Alex Antropoff <alantr@gmail.com>
2023-03-27 00:51:47 +0300

    1st

--- task9/ViewController.swift
+++ task9/ViewController.swift
@@ -7,13 +7,97 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+class CustomFlowLayout: UICollectionViewFlowLayout {
+    override init() {
+        super.init()
+        scrollDirection = .horizontal
+        minimumLineSpacing = 20
+        minimumInteritemSpacing = 20
     }
-
-
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
 
+class CustomCollectionViewCell: UICollectionViewCell {
+    static let reuseIdentifier = "CustomCollectionViewCell"
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupView()
+    }
+    
+    private func setupView() {
+        layer.cornerRadius = 10 // Задайте значение, которое соответствует желаемому радиусу закругления
+        layer.masksToBounds = true
+    }
+}
+
+
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    private var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        navigationItem.title="Collection"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let customFlowLayout = CustomFlowLayout()
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: customFlowLayout)
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.register(CustomCollectionViewCell.self, forCellWithReuseIdentifier: CustomCollectionViewCell.reuseIdentifier)
+        
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(collectionView)
+    }
+    override func viewWillLayoutSubviews() {
+        NSLayoutConstraint.activate([
+            collectionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            //          collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor,constant: 50),
+            //         collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor,constant: -50)
+            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor,constant: 100),
+            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor,constant: -100)
+        ])
+        
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CustomCollectionViewCell.reuseIdentifier, for: indexPath) as! CustomCollectionViewCell
+        cell.backgroundColor = .gray.withAlphaComponent(0.2)
+        return cell
+    }
+    let itemSize = CGSize(width: 250, height:450)
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return itemSize
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        let leftInset = collectionView.layoutMargins.left
+        return UIEdgeInsets(top: 0, left: leftInset, bottom: 0, right: 0)
+    }
+    
+    func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+        let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        let cellWidthIncludingSpacing = itemSize.width + layout.minimumLineSpacing
+        let offset = targetContentOffset.pointee.x
+        print(offset)
+        print(cellWidthIncludingSpacing)
+        let index = round(offset / cellWidthIncludingSpacing)
+        print(index)
+        
+        targetContentOffset.pointee.x = index * cellWidthIncludingSpacing - scrollView.contentInset.left
+    }
+}
